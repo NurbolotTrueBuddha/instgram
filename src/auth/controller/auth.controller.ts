d28f46ea2632b07f50909c5fcfa51eecfe8d7f57
Ulan Sulaimanov <ulan.sulaimanov@latoken.com>
2023-03-01 18:56:53 +0600

    test

---
+++ src/auth/controller/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller()
+export class AuthController {
+
+    
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
+  
+}
